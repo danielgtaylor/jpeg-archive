1efbcead253dc4b6e9b4a5ca1e6d3ff818b327d2
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-07-17 19:54:25 -0700

    Fetch test files on demand

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 
 mkdir -p test-output
 
+if [ ! -d test-files ]; then
+    wget https://www.dropbox.com/s/hb3ah7p5hcjvhc1/jpeg-archive-test-files.zip
+    unzip jpeg-archive-test-files.zip
+fi
+
 for file in test-files/*; do
     ./jpeg-recompress "$file" "test-output/`basename $file`"
 done
