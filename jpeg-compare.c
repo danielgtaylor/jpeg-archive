a0a811ba3138093c72fd73c51d7d9535b6edc994
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-07-20 17:14:13 -0700

    Version updates

--- jpeg-compare.c
+++ jpeg-compare.c
@@ -163,7 +163,7 @@ int main (int argc, char **argv) {
 
     // Parse commandline options
     command_t cmd;
-    command_init(&cmd, argv[0], "1.1.0");
+    command_init(&cmd, argv[0], "2.0.0");
     cmd.usage = "[options] image1.jpg image2.jpg";
     command_option(&cmd, "-s", "--size [arg]", "Set fast comparison image hash size", setSize);
     command_option(&cmd, "-m", "--method [arg]", "Set comparison method to one of 'fast', 'psnr', 'ssim', or 'ms-ssim' [fast]", setMethod);
