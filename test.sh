3208f59fee88803313abe0cc84e15566c79675ae
Daniel G. Taylor <danielgtaylor@gmail.com>
2015-04-07 20:24:32 -0700

    Prefer curl over wget for test data as it is available in more places by default

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p test-output
 
 if [ ! -d test-files ]; then
-    wget https://www.dropbox.com/s/hb3ah7p5hcjvhc1/jpeg-archive-test-files.zip
+    curl -O -L https://www.dropbox.com/s/hb3ah7p5hcjvhc1/jpeg-archive-test-files.zip
     unzip jpeg-archive-test-files.zip
 fi
 
